7a9e43f19fa8edeb5cdee60e248f0221af3e77c2
Mark Kostevych <markkostevych@gmail.com>
2022-08-03 20:16:00 +0300

    server fix

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
+const path = require('path')
 const port = process.env.PORT || 8080
 
 const app = express()
 
-app.use(express.static(__dirname, 'dist'))
+app.use(express.static(path.resolve(__dirname, 'dist')))
 
 app.listen(port, () => {
     console.log(`front listening on port ${port}`)
